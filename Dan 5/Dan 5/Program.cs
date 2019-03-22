cbbe7a1188c416b7d4e787d25741f5dba63f79b6
Caba Varga <caba.varga@outlook.com>
2019-03-22 23:07:16 +0100

    j

---
+++ Dan 5/Dan 5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A ovojeA = new A();
+            A ovojeBkaoA = new B();
+            B ovojeB = new B();
+
+            ovojeA.M();
+            ovojeBkaoA.M();
+            ovojeB.M();
+
+            Test nt = new Test();
+            nt.Prinit();
+        }
+    }
+
+    public class A
+    {
+        public void M()
+        {
+            Console.WriteLine("Ovo pise A");
+        }
+    }
+
+    public class B : A
+    {
+        public void M()
+        {
+            Console.WriteLine("Ovo pise B");
+        }
+    }
+
+    public abstract class C
+    {
+        //public string Name { get; set; }
+
+        //public abstract void PrintName();
+
+        public void Ispis()
+        {
+            Console.WriteLine("Moze??");
+        }
+    }
+
+    public class M : IIme { public virtual void Hello() { } public void Ime() { } }
+    public class N : M { }
+    public class O : N { public override void Hello() { } }
+
+    public interface IIme
+    {
+        void Ime();
+    }
+
+    public class Test
+    {
+        public void Prinit()
+        {
+            IIme mojN = new N();
+            var mojM = (M)mojN;
+            Console.ReadKey();
+        }
+    }
+}
